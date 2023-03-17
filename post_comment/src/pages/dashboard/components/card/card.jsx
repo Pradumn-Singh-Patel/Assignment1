a3501b594273b9d3c79a292ba8327b350bc6a735
Pradumn-Singh-Patel <pradumnpatel18@gmail.com>
2023-03-17 17:50:12 +0530

    fetching post

---
+++ post_comment/src/pages/dashboard/components/card/card.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./card.css"
+
+const card = () => {
+  return (
+    <div className='card'>
+        <div className='title'>this is title</div>
+        <div className='body'>this is body</div>
+    </div>
+  )
+}
+
+export default card
--- post_comment/src/pages/dashboard/dashboard.jsx
+++ post_comment/src/pages/dashboard/dashboard.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
-import "./dashboard.css"
-function dashboard() {
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
+import './dashboard.css'
+import Card from './components/card/card'
+const Dashboard = () => {
+  console.log("hiii")
+    const [Info, setInfo] = useState([])
+    const postUrl='https://jsonplaceholder.typicode.com/posts'
+    const commentUrl='https://jsonplaceholder.typicode.com/comments'
+    useEffect(() => {
+    
+      const getPost= async()=>{
+        const response=await axios.get(postUrl)
+        setInfo(response.data)
+      }
+      getPost()
+    }, [])
+    console.log(Info)
+    
   return (
     <div>
-        <nav>
-        <div className='post' >
-            <h4>Post</h4>
-            </div>
-        <div>
-            <h4 className='comment'>Comment</h4>
+    <nav>
+    <div className='post' >
+        <h3>Post</h3>
         </div>
-        </nav>
+    <div>
+        <h3 className='comment'>Comment</h3>
     </div>
+    
+    </nav>
+    {Info.map((postData)=>
+    <Card info={Info}/>
+    )}
+</div>
   )
 }
 
-export default dashboard
+export default Dashboard
